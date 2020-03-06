f3b395abcaa0ea366132db293f34c83bb4613848
Bhawan Sidhu <55447886+bhawansidhu@users.noreply.github.com>
2020-03-06 16:45:34 -0500

    css changes

--- public/js/pages/Programs.js
+++ public/js/pages/Programs.js
@@ -32,29 +32,18 @@ export default class Programs extends Page {
         <div class="location"><p>North</p></div>
       </ul>
       <ul class="program-row router">
-        <li class="program-name"><a href="Programs/arts">Arts and Media</a></li>
+        <li class="program-name"><a href="Programs/wireless">Wireless Telecommunications</a></li>
         <div class="length"><p>4 Semester</p></div>
         <div class="location"><p>North</p></div>
       </ul>
       <ul class="program-row router">
-        <li class="program-name"><a href="Programs/accounts">Accounting</a></li>
+        <li class="program-name"><a href="Programs/multimedia">Multimedia Design and development</a></li>
         <div class="length"><p>4 Semester</p></div>
         <div class="location"><p>North</p></div>
       </ul>
-      <ul class="program-row router">
-        <li class="program-name"><a href="#">Wireless Telecommunications</a></li>
-        <div class="length"><p>4 Semester</p></div>
-        <div class="location"><p>North</p></div>
-      </ul>
-      <ul class="program-row router">
-        <li class="program-name"><a href="#">Multimedia Design and development</a></li>
-        <div class="length"><p>3 Semester</p></div>
-        <div class="location"><p>North</p></div>
-      </ul>
+      
       <article id="info"></article>
-    </section>
-  
-		`
+    </section>	`
     }
     registerListeners() {
         console.log('registerListeners() from: Programs')
@@ -69,29 +58,57 @@ export default class Programs extends Page {
 
         // Depending on the first directive in the URL, load up different content (these can also be Page components!)
         if (path[0] == 'webdesign') {
-            $info.innerHTML = `<h3>Web Design and Development</h3>
+            $info.innerHTML = `<article class="program-desc">
+            <div class="program-image1"></div>
+            <div class="program-highlight">
+              <h2>Web Design And Development</h2>
+              <p>CERTIFICATE OF ACHIEVEMENT | CODE: WDD911</p>
+            </div>
+          
             <div class="about-pro">
-            <h3>About Web Design and Development Program </h3>
+            <strong> <h3>About the Program </h3></strong>
             <p>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
             You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.</p>
-          </div>`
-        } else if (path[0] == 'arts') {
-            $info.innerHTML = `<h3>Arts and Media</h3>
+          </div>
+          </article>`
+
+        }
+        
+        else if (path[0] == 'wireless') {
+      
+            $info.innerHTML = `<article class="program-desc">
+            <div class="program-image2"></div>
+            <div class="program-highlight">
+              <h2>Wireless Telecommunication</h2>
+              <p>CERTIFICATE OF ACHIEVEMENT | CODE: WMC711</p>
+            </div>
+          
             <div class="about-pro">
-            <h3>About Arts and Media Program </h3>
+            <strong> <h3>About the Program </h3></strong>
             <p>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
             You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
            This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.</p>
-          </div>`
-        } else if (path[0] == 'accounts') {
-            $info.innerHTML = `<h3>Accounting</h3>
-          <div class="about-pro">
-          <h3>About Accounting </h3>
-          <p>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
-          You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
-         This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.</p>
-        </div>`
+          </div>
+          </article>`
+
+
+
+        } else if (path[0] == 'multimedia') {
+            $info.innerHTML = `<article class="program-desc">
+            <div class="program-image3"></div>
+            <div class="program-highlight">
+              <h2>Multimedia Design and development</h2>
+              <p>CERTIFICATE OF ACHIEVEMENT | CODE: MDD811</p>
+            </div>
+          
+            <div class="about-pro">
+            <strong> <h3>About the Program </h3></strong>
+            <p>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
+            You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career.
+           This program is not eligible for Ontario Student Assistance Program (OSAP). It is eligible for Second Career Funding. Further details and how to apply are found at ontario.ca/page/second-career.</p>
+          </div>
+          </article>`
         }
 
     }
