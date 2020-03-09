7be7e5df19b371d4fbc83d6661c025a1d685040a
Bhawanpreet Sidhu <n01372787@nj-00118-014.humber.org>
2020-03-09 17:11:38 -0400

    changes in program page

--- public/js/pages/Programs.js
+++ public/js/pages/Programs.js
@@ -9,10 +9,11 @@ export default class Programs extends Page {
 			<button id="dosomething" style="display:none;">Click me!</button>
     </header>
     <section class="Program-page">
-      <div class="program-image"> </div>
+      <div class="program-image"> 
       <div class="heading-pro">
         <p>CONTINUING EDUCATION</p>
         <h2>Programs List</h2>
+      </div>
       </div>
     </section>
     <section class="program-list">
@@ -59,12 +60,12 @@ export default class Programs extends Page {
         // Depending on the first directive in the URL, load up different content (these can also be Page components!)
         if (path[0] == 'webdesign') {
             $info.innerHTML = `<article class="program-desc">
-            <div class="program-image1"></div>
+            <div class="program-image1">
             <div class="program-highlight">
               <h2>Web Design And Development</h2>
               <p>CERTIFICATE OF ACHIEVEMENT | CODE: WDD911</p>
             </div>
-          
+            </div>
             <div class="about-pro">
             <strong> <h3>About the Program </h3></strong>
             <p>You will be constantly challenged to learn more through the escalating curriculum, while working in our dedicated computer lab. You will be guided by experienced faculty and industry professionals who are passionate about the web and related media in a practical, hands-on style of teaching. Opportunities to network with potential employers, alumni and other professionals will help you to build your own network of invaluable contacts to shape your future career. If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. 
@@ -78,12 +79,12 @@ export default class Programs extends Page {
         else if (path[0] == 'wireless') {
       
             $info.innerHTML = `<article class="program-desc">
-            <div class="program-image2"></div>
+            <div class="program-image2">
             <div class="program-highlight">
               <h2>Wireless Telecommunication</h2>
               <p>CERTIFICATE OF ACHIEVEMENT | CODE: WMC711</p>
             </div>
-          
+            </div>
             <div class="about-pro">
             <strong> <h3>About the Program </h3></strong>
             <p>You will  learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
@@ -96,12 +97,12 @@ export default class Programs extends Page {
 
         } else if (path[0] == 'multimedia') {
             $info.innerHTML = `<article class="program-desc">
-            <div class="program-image3"></div>
+            <div class="program-image3">
             <div class="program-highlight">
               <h2>Visual and Digital Arts </h2>
               <p>CERTIFICATE OF ACHIEVEMENT | CODE: MDD811</p>
             </div>
-          
+            </div>
             <div class="about-pro">
             <strong> <h3>About the Program </h3></strong>
             <p>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you. You will learn to critically analyze problems and apply best practices to plan, design and build websites/applications. You will build sites and apps from the ground up using the most up-to-date techniques and fundamental technologies. You will also learn to tackle technical challenges by developing strong troubleshooting and problem solving skills. The solutions you create along your journey will be curated into a captivating portfolio website at the end of the program.
